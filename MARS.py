e441f98cfaced0b775e18a3dffaabed6c012d9b2
TheRealKS <koen.sauren@gmail.com>
2022-01-08 23:32:15 +0100

    Trying something

--- MARS.py
+++ MARS.py
@@ -4,12 +4,13 @@ from BaseFunction import BaseFunction, HingeFunctionBaseFunction
 from MARSModel import MARSModel
 
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 import numpy as np
 
 MAXVAL = sys.maxsize
 
 
-def runMARS(self, X, n, r, maxSplits):
+def runMARS(self, X, y, n, r, maxSplits):
     model = MARSModel(computeIntercept(X))
 
     for M in range(1, maxSplits, 2):
@@ -17,39 +18,26 @@ def runMARS(self, X, n, r, maxSplits):
         m_ = None
         v_ = None
         t_ = None
+        G = np.zeros(M+1, r)
         for m in range(1, M - 1):  # For all existing terms
             for v in range(1, n):  # For all vars
                 rows = check_nonzero(model.get_component(m), X[v], r)
                 for j in rows:  # For all values of current var where basis func evals positive
                     t = X[v][j]
+                    G[M] = [model.get_component(m).get_function().getvalue(X) * max(0, (k - t)) for k in X[v]]
+                    G[M+1][t] = [model.get_component(m).get_function().getvalue(X) * max(0, (t - k)) for k in X[v]]
 
-                    reg = LinearRegression().fit()
+        #Regress
+        g: LinearRegression = LinearRegression()
+        g.fit(G, y)
+        #Get coeffecients and compute squared-error loss for all sub-models
+        beta = g.coef_
+        y_hat = G * beta
+        lof = mean_squared_error(y, y_hat)
+        #Add to model
 
-                    # g = 0  # Start summing
-                    # for k in range(1, M):
-                    #     # a_i * B_i(x)
-                    #     a = model.get_component(k)
-                    #     term1 = a.get_coefficient() * a.get_function().getvalue(t)
-                    #     #a_M * B_m(x) * max(0, x_v - t)
-                    #     b = model.get_component(M)
-                    #     c = model.get_component(m)
-                    #     d = HingeFunctionBaseFunction(t, v, True)
-                    #     term2 = b.get_coefficient() * c.get_function().getvalue(t) * d.getvalue(t)
-                    #     #a_M+1 * B_m(x) * max(0, t - x_v)
-                    #     e = model.get_component(M+1)
-                    #     f = HingeFunctionBaseFunction(t, v, False)
-                    #     term3 = e.get_coefficient() * c.get_function().getvalue(t) * f.getvalue(t)
-                    #     g += (term1 + term2 + term3)
-                    # #Compute LOF
-                    # lof = LOF(g)
-                    # if (lof < lof_):
-                    #     lof_ = lof
-                    #     m_ = m
-                    #     v_ = v
-                    #     t_ = t
 
-            #Add new hinge function
-            comp1 = model.get_component(m_)
+        comp1 =model.get_component(m_)
 
 
 
@@ -66,4 +54,4 @@ def check_nonzero(func: BaseFunction, v, r):
 
 
 def computeIntercept(X):
-    return 0.0
+    return 1.0
